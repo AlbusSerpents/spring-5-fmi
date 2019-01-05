5cc80b6746fdb03d569312f77a5fc81e454cfa1b
AlbusSerpents <naidim@abv.bg>
2019-01-05 20:40:16 +0200

    Started backend implementation

---
+++ src/main/java/com/ddd/books/in/spring/BooksInSpringApplication.java
@@ -0,0 +1,14 @@
+package com.ddd.books.in.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksInSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksInSpringApplication.class, args);
+    }
+
+}
+
