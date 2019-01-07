a57ffceb8f442ec5e9e59ead33304a89f80aabbc
AlbusSerpents <naidim@abv.bg>
2019-01-07 04:01:03 +0200

    Connected the authentication with the database

--- src/main/java/com/ddd/books/in/spring/auth/UsersAuthenticationProvider.java
+++ src/main/java/com/ddd/books/in/spring/auth/UsersAuthenticationProvider.java
@@ -2,6 +2,7 @@ package com.ddd.books.in.spring.auth;
 
 import com.ddd.books.in.spring.configuration.security.SecurityRole;
 import com.ddd.books.in.spring.func.users.User;
+import com.ddd.books.in.spring.func.users.UsersRepository;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,50 +10,65 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.ddd.books.in.spring.auth.PasswordEncoder.encodePassword;
 import static com.ddd.books.in.spring.configuration.security.SecurityRole.USER;
 import static java.util.Collections.singletonList;
-import static java.util.UUID.randomUUID;
 
-// @TODO replace this with a real configuration after that
 @Component
 public class UsersAuthenticationProvider implements AuthenticationProvider {
 
-    private static final String EXPECTED_EMAIL = "pesho@example.com";
-    private static final String EXPECTED_PASSWORD = encodePassword("1234");
     private static final List<SecurityRole> ROLES = singletonList(USER);
+
+    private final UsersRepository usersRepository;
+
+    public UsersAuthenticationProvider(final UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
     @Override
     public Authentication authenticate(final Authentication authentication) {
-        if (validateCredentials(authentication)) {
-            final User user = new User(
-                    randomUUID(),
-                    "name",
-                    EXPECTED_EMAIL,
-                    EXPECTED_PASSWORD);
 
-            final CustomUserDetails principal = new CustomUserDetails(user, ROLES);
-            return new UsernamePasswordAuthenticationToken(
-                    principal,
-                    authentication.getCredentials(),
-                    principal.getAuthorities());
-        } else {
-            final String message = String.format(
-                    "Couldn't log in with: %s and credentials: %s",
-                    authentication.getName(),
-                    authentication.getCredentials());
-
-            throw new BadCredentialsException(message);
-        }
+        return findUser(authentication)
+                .filter(user -> hasValidCredentials(user, authentication))
+                .map(user -> new CustomUserDetails(user, ROLES))
+                .map(details -> authenticate(details, authentication))
+                .orElseThrow(() -> failure(authentication));
     }
 
-    private boolean validateCredentials(final Authentication authentication) {
-        final String username = authentication.getName();
+    private Optional<User> findUser(final Authentication authentication) {
+        final String email = authentication.getName();
+        return usersRepository.findByEmail(email);
+    }
+
+    private Authentication authenticate(
+            final CustomUserDetails details,
+            final Authentication authentication) {
+        return new UsernamePasswordAuthenticationToken(
+                details,
+                authentication.getCredentials(),
+                details.getAuthorities());
+    }
+
+    private BadCredentialsException failure(final Authentication authentication) {
+        final String message = String.format(
+                "Couldn't log in with: %s and credentials: %s",
+                authentication.getName(),
+                authentication.getCredentials());
+
+        return new BadCredentialsException(message);
+    }
+
+    private boolean hasValidCredentials(
+            final User user,
+            final Authentication authentication) {
         final String password = (String) authentication.getCredentials();
         final String encodedPassword = encodePassword(password);
 
-        return EXPECTED_EMAIL.equals(username) && EXPECTED_PASSWORD.equals(encodedPassword);
+        final String userPassword = user.getPassword();
+
+        return userPassword.equals(encodedPassword);
     }
 
     @Override
--- src/main/java/com/ddd/books/in/spring/func/users/UsersRepository.java
+++ src/main/java/com/ddd/books/in/spring/func/users/UsersRepository.java
@@ -3,6 +3,7 @@ package com.ddd.books.in.spring.func.users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -10,4 +11,7 @@ public interface UsersRepository extends MongoRepository<User, UUID> {
 
     @Query("{$or: [{name: '?0'}, {email: '?1'}]}")
     Stream<User> findByExisting(final String name, final String email);
+
+    @Query("{email: '?0'}")
+    Optional<User> findByEmail(final String email);
 }
