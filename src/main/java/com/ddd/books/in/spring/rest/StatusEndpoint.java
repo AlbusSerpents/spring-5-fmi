5bbdf58d77579d5907da0e34fca0b097df408bc9
AlbusSerpents <naidim@abv.bg>
2019-01-08 07:00:39 +0200

    Changed the Response status of /status to 418

--- src/main/java/com/ddd/books/in/spring/rest/StatusEndpoint.java
+++ src/main/java/com/ddd/books/in/spring/rest/StatusEndpoint.java
@@ -2,13 +2,16 @@ package com.ddd.books.in.spring.rest;
 
 import com.ddd.books.in.spring.func.status.ApplicationStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.I_AM_A_TEAPOT;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class StatusEndpoint {
 
+    @ResponseStatus(I_AM_A_TEAPOT)
     @RequestMapping(value = "/status", method = GET)
     public ApplicationStatus getStatus() {
         return new ApplicationStatus("Books in configuration", "1.0.0", "OK");
