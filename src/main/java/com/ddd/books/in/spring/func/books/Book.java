739ac4f967f1edbf2d0fb0a3064d411cde20b40e
AlbusSerpents <naidim@abv.bg>
2019-01-09 21:44:30 +0200

    Defined the book entity

---
+++ src/main/java/com/ddd/books/in/spring/func/books/Book.java
@@ -0,0 +1,52 @@
+package com.ddd.books.in.spring.func.books;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.UUID;
+
+@SuppressWarnings("WeakerAccess")
+@Data
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private UUID id;
+
+    private String name;
+    private String author;
+    private Integer publishingYear;
+
+    private String description;
+    private List<Marker> contents;
+
+    private List<Rating> ratings;
+    private List<Comment> comments;
+
+    @Data
+    @Document
+    @NoArgsConstructor
+    public static class Marker {
+        private String name;
+        private Integer page;
+    }
+
+    @Data
+    @Document
+    @NoArgsConstructor
+    public static class Comment {
+        private UUID commenterId;
+        private String comment;
+    }
+
+    @Data
+    @Document
+    @NoArgsConstructor
+    public static class Rating {
+        private UUID ratedBy;
+        private Integer rating;
+    }
+}
